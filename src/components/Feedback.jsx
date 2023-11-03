2465fc0ce5cdf3ff4befa6562ce201e465441d1f
Yurii Voloshyn <voloshyn9900@gmail.com>
2023-11-03 22:40:27 +0100

    commit4

--- src/components/Feedback.jsx
+++ src/components/Feedback.jsx
@@ -33,12 +33,17 @@ export class Feedback extends Component {
   }
 
   countPositiveFeedbackPercentage() {
-    const { good, neutral, bad } = this.state;
-    return Math.round((100 / (good + neutral + bad)) * good);
+    const { good } = this.state;
+    const totalFeedback = this.countTotalFeedback();
+    const positivePercentage =
+      totalFeedback === 0 ? 0 : Math.round((good / totalFeedback) * 100);
+    return positivePercentage;
   }
 
   render() {
+    const { good, neutral, bad } = this.state;
     const totalFeedback = this.countTotalFeedback();
+    const positivePercentage = this.countPositiveFeedbackPercentage();
 
 
     return (
@@ -58,20 +63,20 @@ export class Feedback extends Component {
         {totalFeedback > 0 ? ( // Проверка на наличие хотя бы одного отзыва
           <StatisticsList>
             <Li>
-              <StatisticsSpan>Good {this.state.good}</StatisticsSpan>
+              <StatisticsSpan>Good {good}</StatisticsSpan>
             </Li>
             <Li>
-              <StatisticsSpan>Neutral {this.state.neutral}</StatisticsSpan>
+              <StatisticsSpan>Neutral {neutral}</StatisticsSpan>
             </Li>
             <Li>
-              <StatisticsSpan>Bad {this.state.bad}</StatisticsSpan>
+              <StatisticsSpan>Bad {bad}</StatisticsSpan>
             </Li>
             <Li>
               <StatisticsSpan>Total {this.countTotalFeedback()}</StatisticsSpan>
             </Li>
             <Li>
               <StatisticsSpan>
-                {this.countPositiveFeedbackPercentage() > 0
+                {positivePercentage > 0
                   ? `Positive feedback ${this.countPositiveFeedbackPercentage()}%`
                   : `Positive statistics is not available`}
               </StatisticsSpan>
